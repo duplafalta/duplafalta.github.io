ba3079ab9c82b203890eb597033f74ba6f094f7a
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-02-13 13:38:11 -0300

    topo

---
+++ topo/js/_vti_cnf/mhrotator.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|29 May 2017 19:29:40 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_timecreated:TR|17 Nov 2016 13:20:01 -0000
+vti_nexttolasttimemodified:TW|29 May 2017 19:25:34 -0000
+vti_backlinkinfo:VX|Meus\\ Sites/dupla-falta-2015/topo.html dupla-falta-2015/index.html
+vti_cacheddtm:TX|29 May 2017 19:30:56 -0000
+vti_filesize:IR|20609
