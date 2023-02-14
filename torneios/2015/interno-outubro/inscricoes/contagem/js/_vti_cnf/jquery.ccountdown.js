a3272d021b0445f011bcfca33188dfaeabbdefbc
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-02-14 07:16:37 -0300

    torneios

---
+++ torneios/2015/interno-outubro/inscricoes/contagem/js/_vti_cnf/jquery.ccountdown.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_timelastmodified:TR|24 Sep 2015 15:26:08 -0000
+vti_timecreated:TR|28 Jul 2015 14:03:00 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_nexttolasttimemodified:TR|28 Jul 2015 14:03:00 -0000
+vti_cacheddtm:TX|24 Sep 2015 15:26:05 -0000
+vti_filesize:IR|1933
+vti_backlinkinfo:VX|dupla-falta-2015/torneios/2015/interno-outubro/inscricoes/contagem/index.html
