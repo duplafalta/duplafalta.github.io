8e05b283037f08490080b36cddcdd458b4156dec
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-02-14 07:33:17 -0300

    zani-joias-2016

---
+++ zani-joias-open/2016/busca/_vti_cnf/highlight.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_timelastmodified:TR|24 Mar 2017 13:23:05 -0000
+vti_timecreated:TR|04 Apr 2016 17:54:52 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_backlinkinfo:VX|dupla-falta-2015/torneios/2016/zani_joias_open/busca/busca.html dupla-falta-2015/zani-joias-open/2016/busca/busca.html dupla-falta-2015/torneios/2016/zani_joias_open/busca.html
+vti_nexttolasttimemodified:TR|04 Apr 2016 17:54:52 -0000
+vti_cacheddtm:TX|04 Apr 2016 17:54:59 -0000
+vti_filesize:IR|2078
