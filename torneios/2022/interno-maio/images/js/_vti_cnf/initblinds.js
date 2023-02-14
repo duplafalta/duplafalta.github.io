f63257ceca7b2853876ebdb454e88e9650a8f0fb
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-02-14 07:25:56 -0300

    torneios-2022

---
+++ torneios/2022/interno-maio/images/js/_vti_cnf/initblinds.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|15 Jan 2019 16:53:00 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|INTRANET\\junior
+vti_timecreated:TR|25 Jul 2016 17:17:44 -0000
+vti_nexttolasttimemodified:TR|14 Jan 2019 10:57:41 -0000
+vti_backlinkinfo:VX|dupla-falta-2015/torneios/2022/interno-maio/index.html dupla-falta-2015/torneios/2019/interno-fevereiro/index.html
+vti_cacheddtm:TX|15 Jan 2019 16:53:00 -0000
+vti_filesize:IR|874
