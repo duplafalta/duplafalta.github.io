e8b85bfe1178c89e34145255dcae824ae2a504a1
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-02-14 07:47:56 -0300

    granex-varios

---
+++ granexopen/inscricao/contagem/js/_vti_cnf/init.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_timelastmodified:TR|26 Jul 2016 12:11:12 -0000
+vti_timecreated:TR|28 Jul 2015 14:03:00 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_nexttolasttimemodified:TR|25 Jul 2016 16:56:45 -0000
+vti_backlinkinfo:VX|dupla-falta-2015/granexopen/inscricao/contagem/index.html
+vti_cacheddtm:TX|25 Jul 2016 16:56:38 -0000
+vti_filesize:IR|4021
---
+++ granexopen/js/_vti_cnf/mhgallery.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_timelastmodified:TR|17 Aug 2018 17:45:34 -0000
+vti_timecreated:TR|03 Apr 2017 16:47:55 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_backlinkinfo:VX|dupla-falta-2015/old/index_copy(1).html dupla-falta-2015/old/index_copy(12).html dupla-falta-2015/index.html dupla-falta-2015/old/principal.html dupla-falta-2015/old/index_24-04-2017.html
+vti_nexttolasttimemodified:TR|03 Apr 2017 16:47:55 -0000
+vti_cacheddtm:TX|17 Aug 2018 17:45:17 -0000
+vti_filesize:IR|40986
