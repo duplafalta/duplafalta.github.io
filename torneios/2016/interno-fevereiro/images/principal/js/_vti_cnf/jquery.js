966e38f11682afb48b60e8d61cd9d3cf09e0d219
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-02-14 07:15:54 -0300

    torneios1

---
+++ torneios/2016/interno-fevereiro/images/principal/js/_vti_cnf/jquery.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|11 Jan 2019 16:58:12 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|INTRANET\\junior
+vti_nexttolasttimemodified:TR|10 Jan 2019 16:53:46 -0000
+vti_timecreated:TR|19 Sep 2016 16:40:11 -0000
+vti_backlinkinfo:VX|dupla-falta-2015/torneios/2016/interno-fevereiro/images/principal/principal.html dupla-falta-2015/torneios/2016/interno-fevereiro/index.html
+vti_cacheddtm:TX|10 Jan 2019 16:53:46 -0000
+vti_filesize:IR|91668
