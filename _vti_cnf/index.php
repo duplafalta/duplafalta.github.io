90cf756809261188bc8d46c9887ebaabfb33b677
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-02-14 07:04:16 -0300

    varios

---
+++ _vti_cnf/index.php
@@ -0,0 +1,22 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|INTRANET\\junior
+vti_modifiedby:SR|INTRANET\\junior
+vti_timelastmodified:TR|09 Jan 2019 15:34:48 -0000
+vti_timecreated:TR|10 Dec 2018 17:30:45 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_timelastindexed:TW|09 Jan 2019 15:34:48 -0000
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TR|10 Dec 2018 17:44:57 -0000
+vti_title:SR|Dupla Falta - Associação de Tenistas em São Gabriel da Palha - ES
+vti_cacheddtm:TX|09 Jan 2019 15:34:48 -0000
+vti_filesize:IR|633
+vti_cachedtitle:SR|Dupla Falta - Associação de Tenistas em São Gabriel da Palha - ES
+vti_cachedlinkinfo:VX|F|http://duplafalta.com.br/player.html F|http://duplafalta.com.br/principal.html
+vti_cachedsvcrellinks:VX|NFHS|http://duplafalta.com.br/player.html NFHS|http://duplafalta.com.br/principal.html
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8
+vti_charset:SR|utf-8
+vti_hasframeset:BR|true
