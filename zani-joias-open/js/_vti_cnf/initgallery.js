5b552b7348d127d9b755d90b39fb18b100eb9b72
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-05-24 09:52:49 -0300

    zanijoiasopen

---
+++ zani-joias-open/js/_vti_cnf/initgallery.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_timelastmodified:TR|17 Aug 2018 17:46:54 -0000
+vti_timecreated:TR|03 Apr 2017 16:35:54 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_backlinkinfo:VX|dupla-falta-2015/old/index_copy(1).html dupla-falta-2015/old/index_copy(12).html dupla-falta-2015/index.html dupla-falta-2015/old/principal.html dupla-falta-2015/old/index_24-04-2017.html
+vti_nexttolasttimemodified:TR|03 Apr 2017 16:37:41 -0000
+vti_cacheddtm:TX|17 Aug 2018 17:46:56 -0000
+vti_filesize:IR|1080
