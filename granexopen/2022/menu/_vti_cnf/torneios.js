6dc859d8106cb07dcd1fc1bb0a66cbe7e001f4c6
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-05-25 07:26:53 -0300

    granex2022

---
+++ granexopen/2022/menu/_vti_cnf/torneios.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|05 May 2022 16:14:27 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_backlinkinfo:VX|dupla-falta-2015/torneios/2022/interno-maio/index.html dupla-falta-2015/torneios/2022/interno-maio/programacao_simples.html
