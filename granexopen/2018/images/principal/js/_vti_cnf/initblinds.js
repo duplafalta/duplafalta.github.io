8755be1f6d60d2cf204ae14d09d62136a62adc4d
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-05-25 07:32:36 -0300

    granex2018

---
+++ granexopen/2018/images/principal/js/_vti_cnf/initblinds.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|15 Aug 2018 10:50:05 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_timecreated:TR|25 Jul 2016 17:17:44 -0000
+vti_nexttolasttimemodified:TR|31 Jul 2018 10:50:04 -0000
+vti_backlinkinfo:VX|dupla-falta-2015/granexopen/2018/index.html
+vti_cacheddtm:TX|15 Aug 2018 10:50:07 -0000
+vti_filesize:IR|874
