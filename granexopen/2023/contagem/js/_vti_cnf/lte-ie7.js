da5ab18fba7b19c8f52eea5a2f32c3429059039e
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-06-27 07:54:42 -0300

    granex-2023

---
+++ granexopen/2023/contagem/js/_vti_cnf/lte-ie7.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_timelastmodified:TR|23 May 2018 15:56:09 -0000
+vti_timecreated:TR|28 Jul 2015 14:03:00 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_nexttolasttimemodified:TR|23 May 2018 15:55:17 -0000
+vti_cacheddtm:TX|23 May 2018 15:56:01 -0000
+vti_filesize:IR|1063
+vti_backlinkinfo:VX|
---
+++ granexopen/2023/images/js/_vti_cnf/mhslideshow.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|15 Jan 2019 16:53:19 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|INTRANET\\junior
+vti_timecreated:TR|25 Jul 2016 17:17:43 -0000
+vti_nexttolasttimemodified:TR|14 Jan 2019 10:57:41 -0000
+vti_backlinkinfo:VX|dupla-falta-2015/torneios/2022/interno-maio/index.html dupla-falta-2015/granexopen/2022/programacao-simples.html dupla-falta-2015/granexopen/2022/chaveamento-dupla-mista.html dupla-falta-2015/torneios/2019/interno-fevereiro/index.html dupla-falta-2015/granexopen/2022/chaveamento-masculino-1.html dupla-falta-2015/granexopen/2022/chaveamento-masculino-2.html dupla-falta-2015/granexopen/2022/chaveamento-masculino-3.html dupla-falta-2015/granexopen/2022/estatisticas.html dupla-falta-2015/granexopen/2022/chaveamento-duplas-masculino-c.html dupla-falta-2015/granexopen/2022/chaveamento-masculino-iniciante.html dupla-falta-2015/granexopen/2022/programacao-duplas.html dupla-falta-2015/granexopen/2022/chaveamento-duplas-masculino-a.html dupla-falta-2015/granexopen/2022/chaveamento-duplas-masculino-b.html dupla-falta-2015/granexopen/2022/chaveamento-infantil.html dupla-falta-2015/granexopen/2022/index.html dupla-falta-2015/granexopen/2022/regulamento.html dupla-falta-2015/granexopen/2022/chaveamento-feminino-1.html dupla-falta-2015/granexopen/2022/chaveamento-feminino-2.html dupla-falta-2015/granexopen/2022/chaveamento-masculino-4.html dupla-falta-2015/granexopen/2022/chaveamento-feminino-iniciante.html dupla-falta-2015/granexopen/2022/chaveamento-masculino-5.html
+vti_cacheddtm:TX|15 Jan 2019 16:53:19 -0000
+vti_filesize:IR|16385
