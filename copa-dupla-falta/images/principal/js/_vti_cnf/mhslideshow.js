0c820e1d2825b72a7d5eb96302eb45c507014814
duplafalta <122282547+duplafalta@users.noreply.github.com>
2023-08-14 10:48:18 -0300

    copa

---
+++ copa-dupla-falta/images/principal/js/_vti_cnf/mhslideshow.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|22 Aug 2016 13:23:44 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_backlinkinfo:VX|dupla-falta-2015/granexopen/2015/index.html dupla-falta-2015/granexopen/2013/index.html dupla-falta-2015/granexopen/images/principal/principal.html dupla-falta-2015/granexopen/index.html dupla-falta-2015/granexopen/2014/index.html dupla-falta-2015/granexopen/2016/index.html
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_timecreated:TR|25 Jul 2016 17:17:43 -0000
+vti_nexttolasttimemodified:TR|25 Jul 2016 17:17:43 -0000
+vti_cacheddtm:TX|22 Aug 2016 13:23:51 -0000
+vti_filesize:IR|16385
