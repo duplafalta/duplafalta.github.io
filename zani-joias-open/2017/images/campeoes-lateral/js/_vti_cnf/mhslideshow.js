3fe5f52533ecdd0c30cebddfa1c0885f9207d58a
Dupla Falta <duplafaltasgp@gmail.com>
2024-04-05 09:57:28 -0300

    zani-2017

---
+++ zani-joias-open/2017/images/campeoes-lateral/js/_vti_cnf/mhslideshow.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|12 Mar 2018 16:30:34 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_nexttolasttimemodified:TR|12 Mar 2018 16:31:54 -0000
+vti_timecreated:TR|13 Mar 2017 11:27:16 -0000
+vti_backlinkinfo:VX|dupla-falta-2015/zani-joias-open/2017/index.html dupla-falta-2015/zani-joias-open/2017/topo.html dupla-falta-2015/zani-joias-open/2017/programacao.html
+vti_cacheddtm:TX|12 Mar 2018 16:31:57 -0000
+vti_filesize:IR|16385
