e1f2a67c299545e22ce7359baf92f2af391f05e9
Dupla Falta <duplafaltasgp@gmail.com>
2024-04-16 15:18:05 -0300

    2022

---
+++ torneios/2022/interno-setembro/menu/_vti_cnf/torneios.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|08 Aug 2022 11:55:03 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_backlinkinfo:VX|dupla-falta-2015/torneios/2022/interno-maio/index.html dupla-falta-2015/torneios/2022/interno-maio/programacao_simples.html
+vti_author:SR|junior\\junior
+vti_modifiedby:SR|junior\\junior
+vti_timecreated:TR|05 May 2022 16:14:27 -0000
+vti_nexttolasttimemodified:TW|05 May 2022 16:14:27 -0000
+vti_cacheddtm:TX|08 Aug 2022 11:55:03 -0000
+vti_filesize:IR|77080
