8f3ab8d1cd06da260e9b8cf1aecdfd1324f60675
Dupla Falta <duplafaltasgp@gmail.com>
2024-07-30 13:09:59 -0300

    site

---
+++ top-5/old/js/_vti_cnf/mhgallery.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|24 Apr 2017 15:43:25 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_backlinkinfo:VX|
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_timecreated:TR|10 Jul 2015 16:38:27 -0000
+vti_nexttolasttimemodified:TR|08 Mar 2017 12:22:20 -0000
+vti_cacheddtm:TX|08 Mar 2017 12:22:56 -0000
+vti_filesize:IR|40986
