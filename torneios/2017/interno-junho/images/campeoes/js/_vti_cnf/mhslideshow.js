2df09e04c81aeb5c2516c1663094d778e4883bcb
Dupla Falta <duplafaltasgp@gmail.com>
2024-07-30 13:13:09 -0300

    2018

---
+++ torneios/2017/interno-junho/images/campeoes/js/_vti_cnf/mhslideshow.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|30 May 2017 14:20:18 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|JUNIOR\\junior
+vti_nexttolasttimemodified:TR|28 Apr 2017 17:35:48 -0000
+vti_timecreated:TR|19 Sep 2016 16:40:04 -0000
+vti_backlinkinfo:VX|Meus\\ Sites/dupla-falta-2015/torneios/2017/interno-junho/principal.html dupla-falta-2015/torneios/2017/interno-junho/images/campeoes/campeoes.html dupla-falta-2015/torneios/2017/interno-junho/index.html
+vti_cacheddtm:TX|30 May 2017 14:21:40 -0000
+vti_filesize:IR|16385
