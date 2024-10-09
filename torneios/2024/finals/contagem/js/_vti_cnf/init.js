73d33e778b2158461d200f7dd8cc3fc561b3604f
Dupla Falta <duplafaltasgp@gmail.com>
2024-10-09 11:14:03 -0300

    finals

---
+++ torneios/2024/finals/contagem/js/_vti_cnf/init.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|JUNIOR\\junior
+vti_modifiedby:SR|junior\\junior
+vti_timelastmodified:TR|06 Mar 2024 13:58:40 -0000
+vti_timecreated:TR|28 Jul 2015 14:03:00 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_nexttolasttimemodified:TR|23 May 2018 15:55:18 -0000
+vti_cacheddtm:TX|23 May 2018 15:55:57 -0000
+vti_filesize:IR|4021
+vti_backlinkinfo:VX|dupla-falta-2015/zani-joias-open/2018/contagem/index.html
